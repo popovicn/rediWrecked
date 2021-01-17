c1b5c46054d9c6f89d011b7a84b84f5fecff1c29
popovicn <popovic0706@gmail.com>
2021-01-17 02:35:53 +0100

    User interface, parallelism fix

--- README.md
+++ README.md
@@ -9,12 +9,50 @@ Check URLs for response status code and redirect location.
   -o string
     	Output file path (default "output.txt")
   -p int
-    	Parallelism (default 40)
+    	Parallelism (default 50)
+  -s	Simple CLI (tab separated)
 ```
 
 #### Example usage
 ```
 > go run rediWrecked.go -i input.txt -o output.txt 
-http://trickest.com	301	https://trickest.com/
-https://trickest.com	200 
+
+    ____ ____ ___  _ _ _ _ ____ ____ ____ _  _ ____ ___  
+    |__/ |___ |  \ | | | | |__/ |___ |    |_/  |___ |  \ 
+    |  \ |___ |__/ | |_|_| |  \ |___ |___ | \_ |___ |__/ 
+
+
+200 https://www.qq.com/
+200 https://www.apple.com/
+302 https://www.microsoft.com/ → https://www.microsoft.com/sr-latn-rs/
+302 https://www.paypal.com/ → https://www.paypal.com/rs/home
+301 https://www.live.com/ → https://outlook.live.com/owa/
+301 https://www.adcash.com/ → https://adcash.com/
+301 https://www.kickass.so/ → http://www.kickass.so/
+200 https://www.wikipedia.org/
+200 https://www.amazon.de/
+301 https://www.vk.com/ → https://vk.com/
+403 https://www.163.com/
+200 https://www.youtube.com/
+
+```
+##### output.txt
+```
+https://www.qq.com/	200	
+https://www.apple.com/	200	
+https://www.microsoft.com/	302	https://www.microsoft.com/sr-latn-rs/
+https://www.amazon.de/	200	
+https://www.kickass.so/	301	http://www.kickass.so/
+https://www.live.com/	301	https://outlook.live.com/owa/
+https://www.adcash.com/	301	https://adcash.com/
+https://www.wikipedia.org/	200	
+https://www.google.co.in/	200	
+https://www.google.co.uk/	200	
+https://www.google.de/	200	
+https://www.google.ru/	200	
+https://www.google.it/	200	
+https://www.google.com.br/	200	
+https://www.sina.com.cn/	200	
+https://www.google.es/	200	
+https://www.youtube.com/	200	
 ```
--- rediWrecked.go
+++ rediWrecked.go
@@ -10,6 +10,15 @@ import (
 	"sync"
 )
 
+var simpleOutput = false
+
+var banner = "\033[31;1m"  + `
+    ____ ____ ___  _ _ _ _ ____ ____ ____ _  _ ____ ___  
+    |__/ |___ |  \ | | | | |__/ |___ |    |_/  |___ |  \ 
+    |  \ |___ |__/ | |_|_| |  \ |___ |___ | \_ |___ |__/ 
+
+` + "\033[0m"
+
 var outputMutex sync.Mutex
 
 func exit(message string) {
@@ -23,8 +32,26 @@ func check(err error) {
 	}
 }
 
-func formatOutput(url, status, redirect string) string {
+func formatFileOutput(url, status, redirect string) string {
 	return url + "\t" + status + "\t" + redirect
+}
+
+func formatCliOutput(url, status, redirect string) string {
+	color := ""
+	statusCode, err := strconv.Atoi(status)
+	if err == nil {
+		switch statusCode / 100 {
+		case 2: color = "\033[32;1m"
+		case 3: color = "\033[34;1m"
+		case 4: color = "\033[31;1m"
+		case 5: color = "\033[31;2m"
+		}
+	}
+	output := color + status + "\033[0m " + url
+	if redirect != "" {
+		output += "\033[34;1m → " + redirect + "\033[0m"
+	}
+	return output
 }
 
 func writeResultLine(line, outputFile string) {
@@ -37,7 +64,7 @@ func writeResultLine(line, outputFile string) {
 	_ = writer.Flush()
 }
 
-func processUrl(url string, outputFile string, outputChannel chan string, wg *sync.WaitGroup){
+func processUrl(url string, outputFile string, outputChannel chan string, wg *sync.WaitGroup, queue chan struct{}){
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
@@ -46,19 +73,31 @@ func processUrl(url string, outputFile string, outputChannel chan string, wg *sy
 	req, err := http.NewRequest("GET", url, nil)
 	response, err := client.Do(req)
 	if err == nil {
-		output := formatOutput(url, strconv.Itoa(response.StatusCode), response.Header.Get("Location"))
-		fmt.Println(output)
+		output := formatFileOutput(url, strconv.Itoa(response.StatusCode), response.Header.Get("Location"))
+		if simpleOutput {
+			fmt.Println(output)
+		} else {
+			fmt.Println(formatCliOutput(url, strconv.Itoa(response.StatusCode), response.Header.Get("Location")))
+		}
 		writeResultLine(output, outputFile)
 	}
+	<- queue
 	wg.Done()
+
 }
 
 func main() {
 	var inputFilePath = flag.String("i", "","Input file path (required)")
-	var parallelism = flag.Int("p", 40, "Parallelism")
+	var parallelism = flag.Int("p", 50, "Parallelism")
 	var outputFilePath = flag.String("o", "output.txt","Output file path")
+	var simple = flag.Bool("s", false, "Simple CLI (tab separated)")
 	flag.Parse()
 
+	if *simple {
+		simpleOutput = true
+	} else {
+		fmt.Println(banner)
+	}
 	if *inputFilePath == "" {
 		exit("Error: input file is required.")
 	}
@@ -70,14 +109,13 @@ func main() {
 	defer inputFile.Close()
 
 	var wg sync.WaitGroup
-	parallelismBlocker := make(chan struct{}, *parallelism)
+	queue := make(chan struct{}, *parallelism)
 	outputChannel := make(chan string, *parallelism + 1)
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
-		parallelismBlocker <- struct{}{}
+		queue <- struct{}{}
 		wg.Add(1)
-		go processUrl(scanner.Text(), *outputFilePath, outputChannel, &wg)
-		<-parallelismBlocker
+		go processUrl(scanner.Text(), *outputFilePath, outputChannel, &wg, queue)
 	}
 	wg.Wait()
 }
